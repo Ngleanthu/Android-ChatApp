fd49fde7c91fa574cfd344d53c6dc57cc56c3a9c
Anh Thư <145783665+Ngleanthu@users.noreply.github.com>
2024-10-13 20:07:20 +0700

    Update Activity (#3)

--- ChatApp/app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
+++ ChatApp/app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
@@ -1,16 +1,12 @@
 package com.example.chatapp.activities;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.chatapp.R;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
