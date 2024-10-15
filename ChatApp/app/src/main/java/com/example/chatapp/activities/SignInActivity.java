b85b0fb071109a1a56143bd0866d35e4da173415
Ngleanthu <ngleanthu@gmail.com>
2024-10-15 21:36:43 +0700

    Done SignUp Switch SignIn

--- ChatApp/app/src/main/java/com/example/chatapp/activities/SignInActivity.java
+++ ChatApp/app/src/main/java/com/example/chatapp/activities/SignInActivity.java
@@ -1,18 +1,58 @@
 package com.example.chatapp.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.example.chatapp.databinding.ActivitySignInBinding;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-import com.example.chatapp.R;
-
+import java.util.HashMap;
 
 public class SignInActivity extends Activity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        FirebaseApp.initializeApp(this);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
 
+    private void setListeners(){
+
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+
+        binding.buttonSignIn.setOnClickListener(v -> addDataToFirestore());
+    }
+
+    private void addDataToFirestore(){
+
+        // Tạo đối tượng Firestore
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+
+        // Tạo dữ liệu muốn thêm
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "Anthu");
+        data.put("last_name", "Ngle");
+
+        // Thêm dữ liệu vào Firestore
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(exception -> {
+                    Log.e("FirestoreError", "Error adding document", exception);
+                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+//        FirebaseFirestore.getInstance().collection("test").add(data);
     }
 }
--- ChatApp/app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
+++ ChatApp/app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
@@ -2,16 +2,19 @@ package com.example.chatapp.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-
-
-import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends Activity {
 
+    private ActivitySignUpBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+    private void setListeners(){
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
     }
 }
