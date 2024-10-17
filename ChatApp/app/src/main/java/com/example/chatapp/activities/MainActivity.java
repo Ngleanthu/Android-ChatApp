39a0c7821ea2ab0a42a058ff8640d39849f8ac8d
Ngleanthu <ngleanthu@gmail.com>
2024-10-17 21:10:14 +0700

    Done load user name to home page

--- ChatApp/app/src/main/java/com/example/chatapp/activities/MainActivity.java
+++ ChatApp/app/src/main/java/com/example/chatapp/activities/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.chatapp.activities;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivityMainBinding;
+import com.example.chatapp.utils.Constants;
+import com.example.chatapp.utils.PreferenceManager;
 
 
 public class MainActivity extends Activity {
-
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadUserDetails();
+    }
+    private void loadUserDetails(){
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
     }
 }
