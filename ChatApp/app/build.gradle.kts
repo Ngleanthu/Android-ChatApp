6ad348f2db5244173bdaae7e50e6bcac9cc39f22
VoKhacThien <thienvo@i-soft.com.vn>
2024-10-20 15:57:54 +0700

    send message

--- ChatApp/app/build.gradle.kts
+++ ChatApp/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-//    alias(libs.plugins.google.gms.google.services)
-    id("com.google.gms.google-services")
+    id("com.google.gms.google-services") // Google Services plugin
 }
 
 android {
@@ -19,7 +18,7 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -27,45 +26,44 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    buildFeatures{
+
+    buildFeatures {
         viewBinding = true
     }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.firebase.firestore)
-    implementation(libs.firebase.storage)
+
+    implementation(platform("com.google.firebase:firebase-bom:32.2.0"))
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-messaging")
+    implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.android.gms:play-services-auth:20.6.0")
+
+    implementation("com.firebaseui:firebase-ui-firestore:8.0.2")
+
+    implementation("androidx.multidex:multidex:2.0.1")
+
+    // UI Libraries
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    implementation("com.intuit.ssp:ssp-android:1.0.6")
+    implementation("com.makeramen:roundedimageview:2.3.0")
+    implementation ("com.google.firebase:firebase-messaging:23.2.0' // Check for the latest version")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    //Scalable Size Unit (support for different screen size)
-    implementation("com.intuit.sdp:sdp-android:1.0.6")
-    implementation("com.intuit.ssp:ssp-android:1.0.6")
-
-    //Rounded ImageView
-    implementation("com.makeramen:roundedimageview:2.3.0")
-
-    implementation ("com.firebaseui:firebase-ui-firestore:8.0.2")
-
-
-    implementation("androidx.multidex:multidex:2.0.1")
-
-    implementation("com.google.firebase:firebase-firestore:25.1.0")
-    implementation("com.google.firebase:firebase-messaging:24.0.2")
-    implementation(platform("com.google.firebase:firebase-bom:32.2.0")) // Sử dụng Firebase BoM
-    implementation("com.google.firebase:firebase-messaging") // Thư viện FCM
-    implementation("com.google.firebase:firebase-firestore-ktx:25.1.0")
-    implementation("com.google.firebase:firebase-auth:21.0.6")
-// Phiên bản mới nhất
-
 }
+
+// Apply the Google Services plugin automatically (no need for apply plugin in Kotlin Script)
--- ChatApp/app/src/main/java/com/example/chatapp/adapter/SearchUserRecyclerAdapter.java
+++ ChatApp/app/src/main/java/com/example/chatapp/adapter/SearchUserRecyclerAdapter.java
@@ -24,24 +24,22 @@ public class SearchUserRecyclerAdapter extends FirestoreRecyclerAdapter<UserMode
     Context context;
 
     public SearchUserRecyclerAdapter(@NonNull FirestoreRecyclerOptions<UserModel> options, Context context){
-    super(options);
-    this.context=context;
-}
-@Override
+        super(options);
+        this.context=context;
+    }
+    @Override
     protected void onBindViewHolder(@NonNull UserModelViewHolder holder, int position, @NonNull UserModel model) {
-    holder.usernameText.setText(model.getName());
-    holder.phoneText.setText(model.getEmail());
-//    if(model.getUserId().equals(FirebaseUtil.getInstance().getUid())){
-//
-//    }) //Xử lí trường hợp search tên chính mình thì thêm chữ "me" 1:50:38
-    holder.itemView.setOnClickListener(v->{
-        Intent intent = new Intent(context, ChatActivity.class);
-        AndroidUtil.passUserModelAsIntent(intent,model);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
-    });
+        holder.usernameText.setText(model.getName());
+        holder.phoneText.setText(model.getEmail());
 
-}
+        holder.itemView.setOnClickListener(v->{
+            Intent intent = new Intent(context, ChatActivity.class);
+            AndroidUtil.passUserModelAsIntent(intent,model);
+           intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+        });
+
+    }
 
     @NonNull
     @Override
@@ -55,10 +53,73 @@ public class SearchUserRecyclerAdapter extends FirestoreRecyclerAdapter<UserMode
         TextView phoneText;
         ImageView profilePic;
         UserModelViewHolder(@NonNull View itemView) {
-    super(itemView);
-    usernameText = itemView.findViewById(R.id.user_name_text);
-    phoneText = itemView.findViewById(R.id.phone_text);
-    profilePic = itemView.findViewById(R.id.profile_pic_image_view);
-}
+            super(itemView);
+            usernameText = itemView.findViewById(R.id.user_name_text);
+            phoneText = itemView.findViewById(R.id.phone_text);
+            profilePic = itemView.findViewById(R.id.profile_pic_image_view);
+        }
     }
 }
+
+//package com.example.chatapp.adapter;
+//
+//import android.content.Context;
+//import android.content.Intent;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//import androidx.annotation.NonNull;
+//
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import com.example.chatapp.R;
+//import com.example.chatapp.activities.ChatActivity;
+//import com.example.chatapp.models.UserModel;
+//import com.example.chatapp.utils.AndroidUtil;
+//import com.example.chatapp.utils.FirebaseUtil;
+//import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+//import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+//
+//public class SearchUserRecyclerAdapter extends FirestoreRecyclerAdapter<UserModel, SearchUserRecyclerAdapter.UserModelViewHolder> {
+//
+//    Context context;
+//
+//    public SearchUserRecyclerAdapter(@NonNull FirestoreRecyclerOptions<UserModel> options, Context context){
+//    super(options);
+//    this.context=context;
+//}
+//@Override
+//    protected void onBindViewHolder(@NonNull UserModelViewHolder holder, int position, @NonNull UserModel model) {
+//    holder.usernameText.setText(model.getName());
+//    holder.phoneText.setText(model.getEmail());
+//
+//    holder.itemView.setOnClickListener(v->{
+//        Intent intent = new Intent(context, ChatActivity.class);
+//        AndroidUtil.passUserModelAsIntent(intent,model);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        context.startActivity(intent);
+//    });
+//
+//}
+//
+//    @NonNull
+//    @Override
+//    public UserModelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view= LayoutInflater.from(context).inflate(R.layout.search_user_recycle_row,parent,false);
+//        return new UserModelViewHolder(view);
+//    }
+//
+//    class UserModelViewHolder extends RecyclerView.ViewHolder {
+//        TextView usernameText;
+//        TextView phoneText;
+//        ImageView profilePic;
+//        UserModelViewHolder(@NonNull View itemView) {
+//    super(itemView);
+//    usernameText = itemView.findViewById(R.id.user_name_text);
+//    phoneText = itemView.findViewById(R.id.phone_text);
+//    profilePic = itemView.findViewById(R.id.profile_pic_image_view);
+//}
+//    }
+//}
